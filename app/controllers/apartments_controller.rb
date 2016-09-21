c34c6c6dca50c8ad99469c41eec881773ea60622
metalgearalex <alex.chavdaroff@gmail.com>
2016-09-21 00:18:41 -0400

    Got each apartment page to load only its own data correctly and dynamically

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,7 +1,23 @@
 class ApartmentsController < ApplicationController #inherits from application_controller.rb
 	def show #this literally only works if you have an html file called show in a folder called apartments
-		@apartments = Apartment.all
+		apartments_array = Apartment.all
 		
+		for index in 0...apartments_array.size
+
+		@apartment = Apartment.find(params[:id] )
+
+		#why does simply writing Apartment.address with no instance variable naming not work?
+		#how am I not just overwriting the apartment data here so that i only ever see my last db entry's info?
+		@address = @apartment.address
+		@price = @apartment.price
+		@VT_Link = @apartment.VT_Link
+
+		end
+		
+		
+		#current working code to get mass spit out of database on page
+			#@apartments = Apartment.all
+
 		#prior working code in def show method
 			#@apartment = Apartment.find(params[:id])
 		
