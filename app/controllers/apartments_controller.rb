7c686cbabccc3ea0beb63ab02989bfbd05a8b660
metalgearalex <alex.chavdaroff@gmail.com>
2016-09-28 22:47:49 -0400

    showall page doesnt work about to rename apartments to apartment so i can create an apartmentS page

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,15 +1,14 @@
 class ApartmentsController < ApplicationController #inherits from application_controller.rb
 	def show #this literally only works if you have an html file called show in a folder called apartments
 				
-		#you can do things like this in rails without explicitly iterating. this one line is equivalent to creating an array and doing a for loop and only then extracting data out of that individual apartment
+		#you can do things like this in rails without explicitly iterating. this one line is equivalent to creating an array 
+		#and doing a for loop and only then extracting data out of that individual apartment
 		@apartment = Apartment.find(params[:id] ) 
 
 		#why does simply writing Apartment.address with no instance variable naming not work? - cuz which apartment are you asking about
 		@address = @apartment.address
 		@price = @apartment.price
-		@VT_Link = @apartment.VT_Link
-
-				
+		@VT_Link = @apartment.VT_Link				
 		
 		#current working code to get mass spit out of database on page
 			#@apartments = Apartment.all
@@ -23,4 +22,11 @@ class ApartmentsController < ApplicationController #inherits from application_co
 			#@apartment = x
   		
   	end
+
+
+	def showall
+	
+
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  root 'application#hello'
+  get 'static_pages/home', to: 'static_pages#home'  
+  # you’re saying get the URL .com/static_pages/home and have it correspond to the code in the controller static_pages and specifically home method
+
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :apartments 
+  resources :apartments
 end
